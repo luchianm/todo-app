86287cd1e351a33cd87a8ad2092abd7289658162
luchianm <mihai.luchian04@gmail.com>
2021-01-14 00:52:48 +0200

    top right menu added with routing included

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,10 +12,19 @@
         </v-toolbar-title>
         <v-spacer></v-spacer>
 <!--      dropdown menu      -->
-        <v-menu>
-          <v-btn flat slot="activator" color="grey">
-            <v-icon>mdi-expand_more</v-icon>
-          </v-btn>
+
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="grey" v-bind="attrs" v-on="on" class="mx-4 v-btn--flat">
+              <v-icon left>mdi-chevron-down</v-icon>
+              <span>Menu</span>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+              <v-list-item-title class="mx-4">{{ link.text }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
         </v-menu>
 
         <v-btn flat color="grey" class="lighten-2">
